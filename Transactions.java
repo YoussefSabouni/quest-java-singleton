d7d7582c9698337ff1c67931e4e22d44da735f7e
Youssef Sabouni <youssefsabouni@icloud.com>
2020-09-04 22:19:55 +0200

    Solved Quest

--- Transactions.java
+++ Transactions.java
@@ -13,7 +13,7 @@ public class Transactions {
     public static void balance() {
 
         // TODO 2 : call singleton and get total
-        int total = 0;
+        int total = Singleton.getInstance().getBank().getTotal();
         System.out.printf("Your balance is:%d%n", total);
     }
 }
